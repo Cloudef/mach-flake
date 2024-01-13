35bf5c871b27cdf4fdb56a1b78f4df23954cedfe
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2024-01-13 11:32:17 +0900

    turn dawn-binary into derivation

--- package.nix
+++ package.nix
@@ -11,14 +11,36 @@ with builtins;
 let
    target = env.lib.resolveTarget zigTarget stdenvNoCC zigPreferMusl;
    mach-binaries = fromJSON (readFile ./mach-binaries.json);
-   dawn-version = mach-binaries."dawn-${target}".ver;
-   dawn-binary = fetchurl {
-      url = "https://github.com/hexops/mach-gpu-dawn/releases/download/${dawn-version}/libdawn_${target}_release-fast.a.gz";
-      hash = mach-binaries."dawn-${target}".lib;
-   };
-   dawn-headers = fetchurl {
-      url = "https://github.com/hexops/mach-gpu-dawn/releases/download/${dawn-version}/headers.json.gz";
-      hash = mach-binaries."dawn-${target}".hdr;
+
+   dawn-binary = let
+      ver = mach-binaries."dawn-${target}".ver;
+      lib = fetchurl {
+         url = "https://github.com/hexops/mach-gpu-dawn/releases/download/${ver}/libdawn_${target}_release-fast.a.gz";
+         hash = mach-binaries."dawn-${target}".lib;
+      };
+      hdr = fetchurl {
+         url = "https://github.com/hexops/mach-gpu-dawn/releases/download/${ver}/headers.json.gz";
+         hash = mach-binaries."dawn-${target}".hdr;
+      };
+   in stdenvNoCC.mkDerivation {
+      name = "mach-gpu-dawn";
+      version = ver;
+      srcs = [ lib hdr ];
+      nativeBuildInputs = [ gzip jq ];
+      phases = [ "installPhase" ];
+      installPhase = ''
+         mkdir -p $out/${ver}/${target}/release-fast
+         (
+            cd $out/${ver}
+            gzip -d -c ${lib} > ${target}/release-fast/libdawn.a
+            gzip -d -c ${hdr} > ${target}/release-fast/headers.json
+            while read -r key; do
+               mkdir -p "$(dirname "$key")"
+               path="$(realpath $key)"
+               jq -r --arg k "$key" '."\($k)"' ${target}/release-fast/headers.json > "$path"
+            done < <(jq -r 'to_entries | .[] | .key' ${target}/release-fast/headers.json)
+         )
+         '';
    };
 in env.package (
    attrs // {
@@ -27,17 +49,8 @@ in env.package (
    NO_ENSURE_GIT = "true";
    # https://github.com/hexops/mach-gpu-dawn/blob/main/build.zig
    postPatch = ''
-      mkdir -p zig-cache/mach/gpu-dawn/${dawn-version}/${target}/release-fast
-      (
-         cd zig-cache/mach/gpu-dawn/${dawn-version}
-         ${gzip}/bin/gzip -d -c ${dawn-binary} > ${target}/release-fast/libdawn.a
-         ${gzip}/bin/gzip -d -c ${dawn-headers} > ${target}/release-fast/headers.json
-         while read -r key; do
-            mkdir -p "$(dirname "$key")"
-            path="$(realpath $key)"
-            ${jq}/bin/jq -r --arg k "$key" '."\($k)"' ${target}/release-fast/headers.json > "$path"
-         done < <(${jq}/bin/jq -r 'to_entries | .[] | .key' ${target}/release-fast/headers.json)
-      )
+      mkdir -p zig-cache/mach
+      ln -s ${dawn-binary} zig-cache/mach/gpu-dawn
       ${attrs.postPatch or ""}
       '';
 })
